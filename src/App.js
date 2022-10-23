72f561e6729b45e2afc73b8e6460ad55b8c016e5
Josh Darling <71955043+Josh-Darling13@users.noreply.github.com>
2022-10-23 03:28:25 -0400

    built first stage of html table for stying data

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import logo from './logo.svg';
 import './App.css';
 import Main from './Main';
+import bootstrap from '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 
 function App() {
   return (
--- src/Main.js
+++ src/Main.js
@@ -15,7 +15,21 @@ export default function Main() {
     <div>
 
         <h1>Covid Stats</h1>
-
+        <div className="row justify-content-center">
+            <div className="col-md-8">
+                <table className='table table-dark'>
+                    <thead>
+                        <tr>
+                            <th>Country</th>
+                            <th>Confirmed</th>
+                            <th>Active</th>
+                            <th>Recovered</th>
+                            <th>Deaths</th>
+                        </tr>
+                    </thead>
+                </table>
+            </div>
+        </div>
     </div>
   )
 }
