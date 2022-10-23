c37e0efd320fd37d292ebd769340225af56d98a0
Josh Darling <71955043+Josh-Darling13@users.noreply.github.com>
2022-10-23 02:28:20 -0400

    added imports and set up useEffect

--- src/Main.js
+++ src/Main.js
@@ -1,6 +1,15 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import axios from 'axios';
+
 
 export default function Main() {
+
+    useEffect(() => {
+
+    }, [])
+    
+    const url = `api.covid19api.com/summary`;
+
   return (
     <div>
 
